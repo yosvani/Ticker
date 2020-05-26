62476820e0e63ab5f00678453f04b071e19f2946
Darryl Nguyen <nguyen.darryl@gmail.com>
2020-05-26 14:40:58 -0700

    react spinner

--- frontend/util/news_api_util.js
+++ frontend/util/news_api_util.js
@@ -1,6 +1,6 @@
 export const fetchNews = ticker => (
   $.ajax({
-    url: `https://newsapi.org/v2/everything?q=${ticker}&apiKey=77647a5fa383459c8140c181509d6438`,
+    url: `https://cors-anywhere.herokuapp.com/https://newsapi.org/v2/everything?q=${ticker}&apiKey=77647a5fa383459c8140c181509d6438`,
     // dataType: 'jsonp',
     method: 'GET'
   })
@@ -8,7 +8,7 @@ export const fetchNews = ticker => (
 
 export const fetchHomeNews = () => (
   $.ajax({
-    url: `https://newsapi.org/v2/everything?q=stocks&apiKey=77647a5fa383459c8140c181509d6438`,
+    url: `https://cors-anywhere.herokuapp.com/https://newsapi.org/v2/everything?q=stocks&apiKey=77647a5fa383459c8140c181509d6438`,
     // dataType: 'jsonp',
     method: 'GET'
   })
